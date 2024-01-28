eca03e544546c1d57b26dfe5d76d39033b31b356
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-01-28 23:31:00 +0900

    Fix: when application is not given, wt.exe was called infinitely

--- newtab.cmd
+++ newtab.cmd
@@ -1,5 +1,10 @@
 @echo off
 setlocal
+if "%1" == "" (
+    echo %0: - Start application in the other tab of WindowsTerminal
+    echo Usage: %0 COMMANDNAME
+    exit /b 0
+)
 if "%CURRENTDIRECTORY%" == "" (
     for /F %%I in ('cd') do set "CURRENTDIRECTORY=%%I"
     for /F %%I in ('where %1') do @"%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe" --window 0 new-tab -- %~dpnx0 %%I %2 %3 %4 %5 %6 %7 %8 %9
